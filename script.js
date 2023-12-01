0476c23706dae4f8b617560b9b30cecba81a0743
Rodrigo Lopes Emidio <rodrigolopesemidio.costa@gmail.com>
2023-12-01 10:52:06 -0300

    Atualização 2.0 Comentários

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function criarAssinatura() {
     img.onload = function() { // Função que define o canvas com suas estruras iniciais na tela, comprimento e largura 
         canvas.width = img.width;
         canvas.height = img.height;
-        ctx.drawImage(img, 0, 0, img.width, img.height); // COmeça a criar na tela de acordo com seu comprimento e largura definidos anteriormente 
+        ctx.drawImage(img, 0, 0, img.width, img.height); // Começa a criar na tela de acordo com seu comprimento e largura definidos anteriormente 
         var font1 = new FontFace("Gilroy-bold", "url('fonts/gilroy-bold.otf')", {}); // Crio duas varuáveis, uma para cada fonte para garantir a sincronização da fonte nova com os elemntos do meu canvas 
         var font2 = new FontFace("Gilroy-light", "url('fonts/gilroy-light.otf')", {});
 
@@ -70,18 +70,22 @@ function criarAssinatura() {
                 var iconX = telefoneX - iconSize + 40; 
                 var iconY = 100 - iconSize / 2; 
 
+                // Inicia o desenho do meu canvas, referente ao ícone do whatsapp 
                 ctx.drawImage(whatsappImg, iconX, iconY, iconSize, iconSize);
 
+
+                // Variável referente a imagem de perfil; 
                 var profilePhotoInput = document.getElementById('profilePhoto');
                 if (profilePhotoInput.files.length > 0) {
                     var file = profilePhotoInput.files[0];
-                    var reader = new FileReader();
+                    var reader = new FileReader(); // Variável reader ficará escutando no meu HTML se alguma foto foi adicionado pelo usuário
 
                     reader.onload = function(e) {
                         var profilePhoto = new Image();
                         profilePhoto.src = e.target.result;
 
-                        profilePhoto.onload = function() {
+                        // Carrega a imagem de perfil pelo usuário
+                        profilePhoto.onload = function() {   
                             var iconSize = 105; 
                             var iconX = 560; // Posição x do ícone (10 pixels da borda esquerda)
                             var iconY = 23; // Posição y do ícone (10 pixels do topo)
@@ -91,7 +95,7 @@ function criarAssinatura() {
                             ctx.beginPath();
                             ctx.ellipse(iconX + iconSize / 2, iconY + iconSize / 2, iconSize / 2, iconSize / 2, 0, 0, Math.PI * 2);
                             ctx.closePath();
-                            ctx.clip(); // Usar a elipse como uma máscara
+                            ctx.clip(); // Usar a elipse como uma máscara junto à foto de perfil; 
 
                             ctx.drawImage(profilePhoto, iconX, iconY, iconSize, iconSize);
                             ctx.restore(); // Restaurar o contexto para remover a máscara elíptica
@@ -101,23 +105,24 @@ function criarAssinatura() {
                             ctx.fillText(cargo, cargoX, 80);
                             var lineHeight = 1; 
                             var lineWidth = 200; 
-                            var lineX = (canvas.width - lineWidth) / 2; 
+                            var lineX = (canvas.width - lineWidth) / 2; // Desenha uma linha na tela, para ficar abaixo do telefone 
                             var lineY = 85;
                             ctx.fillRect(lineX, lineY, lineWidth, lineHeight);
                             ctx.font = telefoneFontSize + "px Gilroy-bold";
                             ctx.fillText(telefone, telefoneX, 105);
 
-                            var finalImg = document.createElement("img");
-                            finalImg.src = canvas.toDataURL("image/png");
-                            container.appendChild(finalImg);
+                            var finalImg = document.createElement("img"); // Gero a minha imagem final a partir do meu canvas 
+                            finalImg.src = canvas.toDataURL("image/png"); // Defino a extensão .png para a imagem 
+                            container.appendChild(finalImg); // Faço o append final para a imagem ser adicionada ao meu conatiner dentro do meu HTML
                             
+                            // Reseto os campos após a assinatura criada 
                             document.getElementById('nomeColaborador').value = '';
                             document.getElementById('cargoColaborador').value = '';
                             document.getElementById('telefoneColaborador').value = '';
                             document.getElementById('profilePhoto').value = '';
                             document.getElementById("desejaAdicionarFoto").checked = false;
 
-
+                            // Variável que cria o meu botão de download 
                             var downloadButton = document.createElement("button");
                             downloadButton.textContent = "Baixar imagem";
                             downloadButton.onclick = function() {
@@ -131,7 +136,8 @@ function criarAssinatura() {
                         };
                     };
                     reader.readAsDataURL(file);
-                } else {
+
+                } else { // Isso irá ocorrer caso o usuário opte por não carregar a foto de perfil 
                     // Desenhar apenas o texto e o ícone do WhatsApp
                     ctx.font = nomeFontSize + "px Gilroy-bold";
                     ctx.fillText(nome, nomeX, 60);
@@ -179,6 +185,8 @@ function criarAssinatura() {
     var checkbox = document.getElementById("desejaAdicionarFoto");
 
 
+
+    // Se o usuário marcar o checkbox para selecionar uma foto de perfil, carregará a imagem com uma mensagem "conectados pela emoção de ir mais longe "
     if (checkbox.checked){
         switch (lojaSelecionada) {
             case 'grupoAgp':
@@ -201,7 +209,7 @@ function criarAssinatura() {
                 break;
         }
 
-    } else {
+    } else { // Se ele não marcar o checkbox irá subir a imagem sem o slogan 
         switch (lojaSelecionada) {
             case 'grupoAgp':
                 img.src = './img/grupoagp_2023.png'; 
